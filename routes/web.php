ec195f85eba144f6568149458703cf27ebc336ef
まいる <masaya843@gmail.com>
2021-09-03 22:51:45 +0900

    20210903_ikeda

--- routes/web.php
+++ routes/web.php
@@ -22,7 +22,7 @@ Route::get('/', [TodoController::class, 'index']);
 
 Route::get('/todo/create', [TodoController::class, 'add']);
 Route::post('/todo/create', [TodoController::class, 'create']);
-Route::get('/todo/update/{id?}', [TodoController::class, 'edit'])->name('todo.edit');
-Route::post('/todo/update/{id?}', [TodoController::class, 'update'])->name('todo.update');
-Route::get('/todo/delete/{id?}', [TodoController::class, 'delete'])->name('todo.delete');
-Route::post('/todo/delete/{id?}', [TodoController::class, 'remove'])->name('todo.remove');
+Route::get('/todo/update', [TodoController::class, 'edit'])->name('todo.edit');
+Route::post('/todo/update', [TodoController::class, 'update'])->name('todo.update');
+Route::get('/todo/delete', [TodoController::class, 'delete'])->name('todo.delete');
+Route::post('/todo/delete', [TodoController::class, 'remove'])->name('todo.remove');
